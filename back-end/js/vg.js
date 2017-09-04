e9be4a77a72394912ef1631189a833070a8f2f40
Nidis4 <nidis4@gmail.com>
2017-09-04 09:10:31 +0300

    Initial Set-up and Documents

---
+++ back-end/js/vg.js
@@ -0,0 +1,174 @@
+$( ".title" ).on( "click", function() {
+  
+  $( "#title" ).css("display","block");
+  $(".title").attr("src","images/title_on.png");
+
+
+  $( "#metadata" ).css("display","none");
+  $(".metadata").attr("src","images/metadata_off.png");
+  $( "#keimeno" ).css("display","none");
+  $(".keimeno").attr("src","images/text_off.png");
+  $( "#tab1" ).css("display","none");
+  $(".tab1").attr("src","images/tab1_off.png");
+  $( "#tab2" ).css("display","none");
+  $(".tab2").attr("src","images/tab2_off.png");
+  $( "#tab3" ).css("display","none");
+  $(".tab3").attr("src","images/tab3_off.png");
+  $( "#tab4" ).css("display","none");
+  $(".tab4").attr("src","images/tab4_off.png");
+  $( "#exeidikeuseis" ).css("display","none");
+  $(".exeidikeuseis").attr("src","images/tab4_off.png");
+});
+
+$( ".metadata" ).on( "click", function() {
+  
+  $( "#metadata" ).css("display","block");
+  $(".metadata").attr("src","images/metadata_on.png");
+
+
+  $( "#title" ).css("display","none");
+  $(".title").attr("src","images/title_off.png");
+  $( "#keimeno" ).css("display","none");
+  $(".keimeno").attr("src","images/text_off.png");
+  $( "#tab1" ).css("display","none");
+  $(".tab1").attr("src","images/tab1_off.png");
+  $( "#tab2" ).css("display","none");
+  $(".tab2").attr("src","images/tab2_off.png");
+  $( "#tab3" ).css("display","none");
+  $(".tab3").attr("src","images/tab3_off.png");
+  $( "#tab4" ).css("display","none");
+  $(".tab4").attr("src","images/tab4_off.png");
+    $( "#exeidikeuseis" ).css("display","none");
+  $(".exeidikeuseis").attr("src","images/tab4_off.png");
+});
+
+$( ".keimeno" ).on( "click", function() {
+  
+  $( "#keimeno" ).css("display","block");
+  $(".keimeno").attr("src","images/text_on.png");
+
+
+  $( "#title" ).css("display","none");
+  $(".title").attr("src","images/title_off.png");
+  $( "#metadata" ).css("display","none");
+  $(".metadata").attr("src","images/metadata_off.png");
+  $( "#tab1" ).css("display","none");
+  $(".tab1").attr("src","images/tab1_off.png");
+  $( "#tab3" ).css("display","none");
+  $( "#tab4" ).css("display","none");
+  $(".tab4").attr("src","images/tab4_off.png");
+  $( "#tab2" ).css("display","none");
+  $(".tab2").attr("src","images/tab4_off.png");
+    $( "#exeidikeuseis" ).css("display","none");
+  $(".exeidikeuseis").attr("src","images/tab4_off.png");
+});
+
+$( ".tab1" ).on( "click", function() {
+  
+  $( "#tab1" ).css("display","block");
+  $(".tab1").attr("src","images/tab1_on.png");
+
+
+  $( "#title" ).css("display","none");
+  $(".title").attr("src","images/title_off.png");
+  $( "#metadatametadata" ).css("display","none");
+  $(".metadata").attr("src","images/metadata_off.png");
+  $( "#keimeno" ).css("display","none");
+  $(".keimeno").attr("src","images/text_off.png");
+  $( "#tab2" ).css("display","none");
+  $(".tab2").attr("src","images/tab2_off.png");
+  $( "#tab3" ).css("display","none");
+  $(".tab3").attr("src","images/tab3_off.png");
+  $( "#tab4" ).css("display","none");
+  $(".tab4").attr("src","images/tab4_off.png");
+    $( "#exeidikeuseis" ).css("display","none");
+  $(".exeidikeuseis").attr("src","images/tab4_off.png");
+});
+
+$( ".tab2" ).on( "click", function() {
+  
+  $( "#tab2" ).css("display","block");
+  $(".tab2").attr("src","images/tab2_on.png");
+
+
+  $( "#title" ).css("display","none");
+  $(".title").attr("src","images/title_off.png");
+  $( "#metadatametadata" ).css("display","none");
+  $(".metadata").attr("src","images/metadata_off.png");
+  $( "#keimeno" ).css("display","none");
+  $(".keimeno").attr("src","images/text_off.png");
+  $( "#tab1" ).css("display","none");
+  $(".tab1").attr("src","images/tab1_off.png");
+  $( "#tab3" ).css("display","none");
+  $(".tab3").attr("src","images/tab3_off.png");
+  $( "#tab4" ).css("display","none");
+  $(".tab4").attr("src","images/tab4_off.png");
+    $( "#exeidikeuseis" ).css("display","none");
+  $(".exeidikeuseis").attr("src","images/tab4_off.png");
+});
+
+$( ".tab3" ).on( "click", function() {
+  
+  $( "#tab3" ).css("display","block");
+  $(".tab3").attr("src","images/tab3_on.png");
+
+
+  $( "#title" ).css("display","none");
+  $(".title").attr("src","images/title_off.png");
+  $( "#metadatametadata" ).css("display","none");
+  $(".metadata").attr("src","images/metadata_off.png");
+  $( "#keimeno" ).css("display","none");
+  $(".keimeno").attr("src","images/text_off.png");
+  $( "#tab1" ).css("display","none");
+  $(".tab1").attr("src","images/tab1_off.png");
+  $( "#tab2" ).css("display","none");
+  $(".tab2").attr("src","images/tab2_off.png");
+  $( "#tab4" ).css("display","none");
+  $(".tab4").attr("src","images/tab4_off.png");
+    $( "#exeidikeuseis" ).css("display","none");
+  $(".exeidikeuseis").attr("src","images/tab4_off.png");
+});
+
+$( ".tab4" ).on( "click", function() {
+  
+  $( "#tab4" ).css("display","block");
+  $(".tab4").attr("src","images/tab4_on.png");
+
+
+  $( "#title" ).css("display","none");
+  $(".title").attr("src","images/title_off.png");
+  $( "#metadatametadata" ).css("display","none");
+  $(".metadata").attr("src","images/metadata_off.png");
+  $( "#keimeno" ).css("display","none");
+  $(".keimeno").attr("src","images/text_off.png");
+  $( "#tab1" ).css("display","none");
+  $(".tab1").attr("src","images/tab1_off.png");
+  $( "#tab3" ).css("display","none");
+  $(".tab3").attr("src","images/tab3_off.png");
+  $( "#tab2" ).css("display","none");
+  $(".tab2").attr("src","images/tab2_off.png");
+  $( "#exeidikeuseis" ).css("display","none");
+  $(".exeidikeuseis").attr("src","images/tab4_off.png");
+});
+
+$( ".exeidikeuseis" ).on( "click", function() {
+  
+  $( "#exeidikeuseis" ).css("display","block");
+  $(".exeidikeuseis").attr("src","images/exeidikeuseis_on.png");
+
+
+  $( "#title" ).css("display","none");
+  $(".title").attr("src","images/title_off.png");
+  $( "#metadatametadata" ).css("display","none");
+  $(".metadata").attr("src","images/metadata_off.png");
+  $( "#keimeno" ).css("display","none");
+  $(".keimeno").attr("src","images/text_off.png");
+  $( "#tab1" ).css("display","none");
+  $(".tab1").attr("src","images/tab1_off.png");
+  $( "#tab3" ).css("display","none");
+  $(".tab3").attr("src","images/tab3_off.png");
+  $( "#tab2" ).css("display","none");
+  $(".tab2").attr("src","images/tab2_off.png");
+  $( "#tab4" ).css("display","none");
+  $(".tab4").attr("src","images/tab4_off.png");
+});
