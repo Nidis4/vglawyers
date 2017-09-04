b49b57e29b6b9614f7bb55345f6450af2ff8f83b
Nidis4 <macuser@Macusers-MacBook-Pro-10.local>
2017-09-04 11:50:11 +0300

    All Pages Creation

--- back-end/js/vg.js
+++ back-end/js/vg.js
@@ -17,7 +17,13 @@ $( ".title" ).on( "click", function() {
   $( "#tab4" ).css("display","none");
   $(".tab4").attr("src","images/tab4_off.png");
   $( "#exeidikeuseis" ).css("display","none");
-  $(".exeidikeuseis").attr("src","images/tab4_off.png");
+  $(".exeidikeuseis").attr("src","images/tabs/expertise_list_off.png");
+  $( "#photo" ).css("display","none");
+  $(".photo").attr("src","images/tabs/foto_off.png");
+  $( "#lawyerslist" ).css("display","none");
+  $(".lawyerslist").attr("src","images/tabs/lawyers_list_off.png");
+  $( "#dimosieuseis" ).css("display","none");
+  $(".dimosieuseis").attr("src","images/tabs/dimosieuseis_off.png");
 });
 
 $( ".metadata" ).on( "click", function() {
@@ -38,8 +44,14 @@ $( ".metadata" ).on( "click", function() {
   $(".tab3").attr("src","images/tab3_off.png");
   $( "#tab4" ).css("display","none");
   $(".tab4").attr("src","images/tab4_off.png");
-    $( "#exeidikeuseis" ).css("display","none");
-  $(".exeidikeuseis").attr("src","images/tab4_off.png");
+  $( "#exeidikeuseis" ).css("display","none");
+  $(".exeidikeuseis").attr("src","images/tabs/expertise_list_off.png");
+  $( "#photo" ).css("display","none");
+  $(".photo").attr("src","images/tabs/foto_off.png");
+  $( "#lawyerslist" ).css("display","none");
+  $(".lawyerslist").attr("src","images/tabs/lawyers_list_off.png");
+  $( "#dimosieuseis" ).css("display","none");
+  $(".dimosieuseis").attr("src","images/tabs/dimosieuseis_off.png");
 });
 
 $( ".keimeno" ).on( "click", function() {
@@ -59,8 +71,10 @@ $( ".keimeno" ).on( "click", function() {
   $(".tab4").attr("src","images/tab4_off.png");
   $( "#tab2" ).css("display","none");
   $(".tab2").attr("src","images/tab4_off.png");
-    $( "#exeidikeuseis" ).css("display","none");
-  $(".exeidikeuseis").attr("src","images/tab4_off.png");
+  $( "#exeidikeuseis" ).css("display","none");
+  $(".exeidikeuseis").attr("src","images/tabs/expertise_list_off.png");
+  $( "#photo" ).css("display","none");
+  $(".photo").attr("src","images/tabs/foto_off.png");
 });
 
 $( ".tab1" ).on( "click", function() {
@@ -81,8 +95,8 @@ $( ".tab1" ).on( "click", function() {
   $(".tab3").attr("src","images/tab3_off.png");
   $( "#tab4" ).css("display","none");
   $(".tab4").attr("src","images/tab4_off.png");
-    $( "#exeidikeuseis" ).css("display","none");
-  $(".exeidikeuseis").attr("src","images/tab4_off.png");
+  $( "#exeidikeuseis" ).css("display","none");
+  $(".exeidikeuseis").attr("src","images/tabs/expertise_list_off.png");
 });
 
 $( ".tab2" ).on( "click", function() {
@@ -103,8 +117,10 @@ $( ".tab2" ).on( "click", function() {
   $(".tab3").attr("src","images/tab3_off.png");
   $( "#tab4" ).css("display","none");
   $(".tab4").attr("src","images/tab4_off.png");
-    $( "#exeidikeuseis" ).css("display","none");
-  $(".exeidikeuseis").attr("src","images/tab4_off.png");
+  $( "#exeidikeuseis" ).css("display","none");
+  $(".exeidikeuseis").attr("src","images/tabs/expertise_list_off.png");
+  $( "#photo" ).css("display","none");
+  $(".photo").attr("src","images/tabs/foto_off.png");
 });
 
 $( ".tab3" ).on( "click", function() {
@@ -125,8 +141,10 @@ $( ".tab3" ).on( "click", function() {
   $(".tab2").attr("src","images/tab2_off.png");
   $( "#tab4" ).css("display","none");
   $(".tab4").attr("src","images/tab4_off.png");
-    $( "#exeidikeuseis" ).css("display","none");
-  $(".exeidikeuseis").attr("src","images/tab4_off.png");
+  $( "#exeidikeuseis" ).css("display","none");
+  $(".exeidikeuseis").attr("src","images/tabs/expertise_list_off.png");
+  $( "#photo" ).css("display","none");
+  $(".photo").attr("src","images/tabs/foto_off.png");
 });
 
 $( ".tab4" ).on( "click", function() {
@@ -148,13 +166,15 @@ $( ".tab4" ).on( "click", function() {
   $( "#tab2" ).css("display","none");
   $(".tab2").attr("src","images/tab2_off.png");
   $( "#exeidikeuseis" ).css("display","none");
-  $(".exeidikeuseis").attr("src","images/tab4_off.png");
+  $(".exeidikeuseis").attr("src","images/tabs/expertise_list_off.png");
+  $( "#photo" ).css("display","none");
+  $(".photo").attr("src","images/tabs/foto_off.png");
 });
 
-$( ".exeidikeuseis" ).on( "click", function() {
+$( ".photo" ).on( "click", function() {
   
-  $( "#exeidikeuseis" ).css("display","block");
-  $(".exeidikeuseis").attr("src","images/exeidikeuseis_on.png");
+  $( "#photo" ).css("display","block");
+  $(".photo").attr("src","images/tabs/foto_on.png");
 
 
   $( "#title" ).css("display","none");
@@ -171,4 +191,59 @@ $( ".exeidikeuseis" ).on( "click", function() {
   $(".tab2").attr("src","images/tab2_off.png");
   $( "#tab4" ).css("display","none");
   $(".tab4").attr("src","images/tab4_off.png");
+  $( "#exeidikeuseis" ).css("display","none");
+  $(".exeidikeuseis").attr("src","images/tabs/expertise_list_off.png");
+});
+
+$( ".exeidikeuseis" ).on( "click", function() {
+  
+  $( "#exeidikeuseis" ).css("display","block");
+  $(".exeidikeuseis").attr("src","images/tabs/expertise_list_on.png");
+
+  $( "#title" ).css("display","none");
+  $(".title").attr("src","images/title_off.png");
+  $( "#metadatametadata" ).css("display","none");
+  $(".metadata").attr("src","images/metadata_off.png");
+  $( "#keimeno" ).css("display","none");
+  $(".keimeno").attr("src","images/text_off.png");
+  $( "#tab1" ).css("display","none");
+  $(".tab1").attr("src","images/tab1_off.png");
+  $( "#tab3" ).css("display","none");
+  $(".tab3").attr("src","images/tab3_off.png");
+  $( "#tab2" ).css("display","none");
+  $(".tab2").attr("src","images/tab2_off.png");
+  $( "#tab4" ).css("display","none");
+  $(".tab4").attr("src","images/tab4_off.png");
+});
+
+$( ".lawyerslist" ).on( "click", function() {
+  
+  $( "#lawyerslist" ).css("display","block");
+  $(".lawyerslist").attr("src","images/tabs/lawyers_list_on.png");
+
+  $( "#title" ).css("display","none");
+  $(".title").attr("src","images/title_off.png");
+  $( "#metadatametadata" ).css("display","none");
+  $(".metadata").attr("src","images/metadata_off.png");
+  $( "#keimeno" ).css("display","none");
+  $(".keimeno").attr("src","images/text_off.png");
+  $( "#tab1" ).css("display","none");
+  $(".tab1").attr("src","images/tab1_off.png");
+  $( "#tab3" ).css("display","none");
+  $(".tab3").attr("src","images/tab3_off.png");
+  $( "#tab2" ).css("display","none");
+  $(".tab2").attr("src","images/tab2_off.png");
+  $( "#tab4" ).css("display","none");
+  $(".tab4").attr("src","images/tab4_off.png");
+});
+
+$( ".dimosieuseis" ).on( "click", function() {
+  
+  $( "#dimosieuseis" ).css("display","block");
+  $(".dimosieuseis").attr("src","images/tabs/dimosieuseis_on.png");
+
+  $( "#title" ).css("display","none");
+  $(".title").attr("src","images/title_off.png");
+  $( "#metadatametadata" ).css("display","none");
+  $(".metadata").attr("src","images/metadata_off.png");
 });
